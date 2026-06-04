9745289579af8c235c0bd89923f0e5d98a3f5a78
matviy <matviy13@proton.me>
2026-06-04 13:39:12 +0300

    feat: formatters

--- apps/web/src/lib/editor/editor-toolbar.svelte
+++ apps/web/src/lib/editor/editor-toolbar.svelte
@@ -1,5 +1,12 @@
 <script module>
     const HEADINGS = /** @type {const} */ ([{ level: 1 }, { level: 2 }, { level: 3 }]);
+
+    const FORMATTERS = /** @type {const} */ ([
+        { type: "bold", toggler: "toggleBold" },
+        { type: "italic", toggler: "toggleItalic" },
+        { type: "underline", toggler: "toggleUnderline" },
+        { type: "strike", toggler: "toggleStrike" },
+    ]);
 </script>
 
 <script>
@@ -14,30 +21,48 @@
     import Heading2 from "$lib/components/ui/icons/heading-2.svelte";
     import Heading3 from "$lib/components/ui/icons/heading-3.svelte";
     import { cn } from "$lib/components/ui/utils";
+    import Separator from "$lib/components/ui/separator.svelte";
+    import Bold from "$lib/components/ui/icons/bold.svelte";
+    import Italic from "$lib/components/ui/icons/italic.svelte";
+    import Strike from "$lib/components/ui/icons/strike.svelte";
+    import Underline from "$lib/components/ui/icons/underline.svelte";
 
     /** @type {Omit<import('svelte').ComponentProps<typeof StickyBar>, 'children'> & { editor: import('@tiptap/core').Editor }} */
     const { editor, ...props } = $props();
 
-    function selectedNodeIsAnyHeading() {
-        return editor.isActive("heading");
+    function getSelectedNodeLabel() {
+        const parent = editor.view.state.selection.$head.parent;
+
+        switch (parent.type.name) {
+            case "heading":
+                return `Heading ${parent.attrs.level}`;
+            default:
+                return "Paragraph";
+        }
     }
 </script>
 
 <StickyBar {...props} class={cn("gap-2", props.class)}>
     <Menu>
         {#snippet trigger(props)}
-            <Action button variant={selectedNodeIsAnyHeading() ? "secondary" : "ghost"} {...props}>
-                <Text />
+            <Action
+                button
+                variant={editor.isActive("heading") ? "secondary" : "outline"}
+                class="w-30"
+                {...props}
+            >
+                <span>{getSelectedNodeLabel()}</span>
                 <ChevronDown />
             </Action>
         {/snippet}
         <MenuContent>
-            {#each HEADINGS as heading}
+            {#each HEADINGS as heading (heading.level)}
                 <MenuActionItem
                     button
                     onclick={() => editor.chain().focus().toggleHeading(heading).run()}
                     variant={editor.isActive("heading", heading) ? "secondary" : undefined}
                     disabled={!editor.can().toggleHeading(heading)}
+                    keepOpen
                 >
                     {#if heading.level === 1}
                         <Heading1 />
@@ -46,7 +71,7 @@
                     {:else if heading.level === 3}
                         <Heading3 />
                     {/if}
-                    <span>Toggle <code>H{heading.level}</code></span>
+                    <span>Heading {heading.level}</span>
                 </MenuActionItem>
             {/each}
 
@@ -55,10 +80,37 @@
                 onclick={() => editor.chain().focus().setParagraph().run()}
                 variant={editor.isActive("paragraph") ? "secondary" : undefined}
                 disabled={!editor.can().setParagraph()}
+                keepOpen
             >
                 <Text />
                 <span>Paragraph</span>
             </MenuActionItem>
         </MenuContent>
     </Menu>
+
+    <Separator orientation="vertical" />
+
+    {#each FORMATTERS as formatter}
+        <Action
+            button
+            size="icon"
+            onclick={() => editor.chain().focus()[formatter.toggler]().run()}
+            variant={editor.isActive(formatter.type) ? "secondary" : "outline"}
+            disabled={!editor.can()[formatter.toggler]()}
+        >
+            {@render formatterIcon(formatter.type)}
+        </Action>
+    {/each}
 </StickyBar>
+
+{#snippet formatterIcon(/** @type {(typeof FORMATTERS)[number]['type']} */ type)}
+    {#if type === "bold"}
+        <Bold />
+    {:else if type === "italic"}
+        <Italic />
+    {:else if type === "strike"}
+        <Strike />
+    {:else if type === "underline"}
+        <Underline />
+    {/if}
+{/snippet}
